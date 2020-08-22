800744de0fb218091c40b6f6bf00cba7d8f6dc9e
Caia Chuang <caiachung@gmail.com>
2020-08-22 21:43:26 +0000

    add group members to the users db

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -5,9 +5,9 @@ VALUES ('Caia', 'caiachuang@gmail.com', 'password', '7786289669');
 
 INSERT INTO users (
 name, email, password, phone)
-VALUES ('Derek', 'derek@gmail.com', 'password', '1234567890');
+VALUES ('Derek', 'derekbutvin@gmail.com', 'password', '1234567890');
 
 INSERT INTO users (
 name, email, password, phone)
-VALUES ('Jenny', 'jenny@gmail.com', 'password', '1234567890');
+VALUES ('Jenny', 'jennycith@gmail.com', 'password', '1234567890');
 
