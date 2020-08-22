22d5943839d7343ccc0dd559cd0019531d4864da
Caia Chuang <caiachung@gmail.com>
2020-08-22 05:23:30 +0000

    add user login from Andy's code on w5d5

--- routes/users.js
+++ routes/users.js
@@ -19,12 +19,9 @@ module.exports = (db) => {
         res.status(500).json({ error: err.message });
       });
   });
-
   router.get('/login/:id', (req, res) => {
     req.session.user_id = req.params.id;
-    // console.log('hello');
     res.redirect('/');
-  });
-
+  })
   return router;
 };
