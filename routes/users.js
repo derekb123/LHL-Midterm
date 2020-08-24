51252a173394bf6da55e064ca6710cf29e5dd100
Caia Chuang <caiachung@gmail.com>
2020-08-24 02:35:46 +0000

    fix the order in db/schema, menu_items should goes before order_items since order_items takes menu_items's id as a reference

--- routes/users.js
+++ routes/users.js
@@ -5,27 +5,25 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
-      .then(data => {
+      .then((data) => {
         const users = data.rows;
         res.json({ users });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
-  router.get('/login/:id', (req, res) => {
+  router.get("/login/:id", (req, res) => {
     req.session.user_id = req.params.id;
     // console.log('hello');
-    res.redirect('/');
+    res.redirect("/");
   });
 
   return router;
