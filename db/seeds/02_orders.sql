db3d53f0970b890e25aba351f55c8e7bcfc6eab7
Caia Chuang <caiachung@gmail.com>
2020-08-25 21:52:43 +0000

    modify db schema and seeds

--- db/seeds/02_orders.sql
+++ db/seeds/02_orders.sql
@@ -1,11 +1,11 @@
 -- Users table seeds here (Example)
 INSERT INTO orders (
 user_id, place_at, order_status, special_instructure)
-VALUES (1, '2020-08-22 19:10:25-07', TRUE, 'no onions');
+VALUES (1, '2020-08-16 19:10:25-07', TRUE, 'no onions');
 
 INSERT INTO orders (
 user_id, place_at, order_status, special_instructure)
-VALUES (3, '2020-08-16 15:10:25-07', TRUE, 'no chilli pepper');
+VALUES (3, '2020-08-22 15:10:25-07', TRUE, 'no chilli pepper');
 
 INSERT INTO orders (
 user_id, place_at, order_status, special_instructure)
