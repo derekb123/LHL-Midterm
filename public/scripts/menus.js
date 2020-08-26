2200a300c636d360c9db9414188614f9be3b232e
Caia Chuang <caiachung@gmail.com>
2020-08-26 17:21:47 +0000

    delete primary key in orderd_items.psql

--- public/scripts/menus.js
+++ public/scripts/menus.js
@@ -1,1 +1,17 @@
 
+$(() => {
+
+
+  function getAllMenuItems(params) {
+    let url = "/api/menu";
+    if (params) {
+      url += "?" + params;
+    }
+    return $.ajax({
+      url,
+    });
+  }
+
+
+
+});
