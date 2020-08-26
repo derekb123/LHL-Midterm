e1ce79c299a2b7a675d0dadff617c5206e7fd4d9
Caia Chuang <caiachung@gmail.com>
2020-08-26 23:22:52 +0000

    create ajax click listener

--- public/scripts/order_button.js
+++ public/scripts/order_button.js
@@ -4,6 +4,15 @@ const sendSms = require('./twilio');
 $(() => {
   $('.cart_footer').click( function() {
     event.preventDefault();
+    $.ajax({
+      method: "POST",
+      url: "/api/orders"
+      data:
+    }).done((users) => {
+      for(user of users) {
+        $("<div>").text(user.name).appendTo($("body"));
+      }
+    });;
     const msg = 'Your order will be ready in 20 minutes';
     console.log('send msg to customer')
     sendSms(msg);
