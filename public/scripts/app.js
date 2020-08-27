968eced38a81fce53ec2f2489ecb499c4f3df122
Caia Chuang <caiachung@gmail.com>
2020-08-27 18:01:17 +0000

    Add id=order-now to order now button to connect submit route and twilio

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,16 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for (user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  //original skeleton
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for (user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });;
 
-  $('#order_button').click( function() {
+  $('#order-now').click( function() {
     event.preventDefault();
-    console.log("in order button");
     $.ajax({
       method: "POST",
       url: "/api/orders/1/submit/"
--- server.js
+++ server.js
@@ -71,7 +71,6 @@ app.get("/", (req, res) => {
     })
 });
 
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
