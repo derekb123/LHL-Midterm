489a775bc25e13aac428582faa314fc53e5561a1
Caia Chuang <caiachung@gmail.com>
2020-08-27 18:01:17 +0000

    Add id=order-now to order now button to connect submit route and twilio

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,6 +9,7 @@ $(() => {
   //   }
   // });;
 
+<<<<<<< 233d344f1d4531f6fbbe7bd7c3831447019bae2d
 <<<<<<< 968eced38a81fce53ec2f2489ecb499c4f3df122
   $('#order-now').click( function() {
     event.preventDefault();
@@ -17,6 +18,10 @@ $(() => {
     event.preventDefault();
     console.log("in order button");
 >>>>>>> Add route and ajex connection for logout.
+=======
+  $('#order-now').click( function() {
+    event.preventDefault();
+>>>>>>> Add id=order-now to order now button to connect submit route and twilio
     $.ajax({
       method: "POST",
       url: "/api/orders/1/submit/"
