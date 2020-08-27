5348f9c54ea97e9ff5abd881006bc39592fa0f18
Caia <caiachuang@gmail.com>
2020-08-27 12:27:04 -0700

    Fix artifacts from merge conflict.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,19 +9,8 @@ $(() => {
   //   }
   // });;
 
-<<<<<<< 233d344f1d4531f6fbbe7bd7c3831447019bae2d
-<<<<<<< 968eced38a81fce53ec2f2489ecb499c4f3df122
   $('#order-now').click( function() {
     event.preventDefault();
-=======
-  $('#order_button').click( function() {
-    event.preventDefault();
-    console.log("in order button");
->>>>>>> Add route and ajex connection for logout.
-=======
-  $('#order-now').click( function() {
-    event.preventDefault();
->>>>>>> Add id=order-now to order now button to connect submit route and twilio
     $.ajax({
       method: "POST",
       url: "/api/orders/1/submit/"
