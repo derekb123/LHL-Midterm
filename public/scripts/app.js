233d344f1d4531f6fbbe7bd7c3831447019bae2d
Caia Chuang <caiachung@gmail.com>
2020-08-27 17:41:57 +0000

    Add route and ajex connection for logout.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,8 +9,14 @@ $(() => {
   //   }
   // });;
 
+<<<<<<< 968eced38a81fce53ec2f2489ecb499c4f3df122
   $('#order-now').click( function() {
     event.preventDefault();
+=======
+  $('#order_button').click( function() {
+    event.preventDefault();
+    console.log("in order button");
+>>>>>>> Add route and ajex connection for logout.
     $.ajax({
       method: "POST",
       url: "/api/orders/1/submit/"
